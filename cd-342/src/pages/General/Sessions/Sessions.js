66740657468ceeb1d34e711f56d1631ab729874c
Marcos Gabriel <marcosindev@gmail.com>
2025-05-10 17:05:01 -0300

    FIX BUG IN SCSS

--- cd-342/src/pages/General/Sessions/Sessions.js
+++ cd-342/src/pages/General/Sessions/Sessions.js
@@ -4,45 +4,27 @@ function Sessions({ nome, ip, mac, patrimônio, setor }) {
 
     return (
         <ul className={styles.object}>
-            <li>
-                <label>
-                    Nome
-                </label>
-                <span>
-                    {nome}
-                </span>
-            </li>
-            <li>
-                <label>
-                    Endereço IP
-                </label>
-                <span>
-                    {ip}
-                </span>
-            </li>
-            <li>
-                <label>
-                    Endereço MAC
-                </label>
-                <span>
-                    {mac}
-                </span>
-            </li>
-            <li>
-                <label>
-                    Patrimônio
-                </label>
-                <span>
-                    {patrimônio}
-                </span>
-            </li>
-            <li>
-                <label>
-                    Setor
-                </label>
-                <span>
-                    {setor}
-                </span>
+            <li className={styles.object_structure}>
+                <div>
+                    <label>Nome</label>
+                    <span>{nome}</span>
+                </div>
+                <div>
+                    <label>Endereço IP</label>
+                    <span>{ip}</span>
+                </div>
+                <div>
+                    <label>Endereço MAC</label>
+                    <span>{mac}</span>
+                </div>
+                <div>
+                    <label>Patrimônio</label>
+                    <span>{patrimônio}</span>
+                </div>
+                <div>
+                    <label>Setor</label>
+                    <span>{setor}</span>
+                </div>
             </li>
         </ul>
     )
