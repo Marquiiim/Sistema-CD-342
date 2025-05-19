e26b9e6a749db363653c1c8c9d219c2e63cade2e
Marcos Gabriel <marcosindev@gmail.com>
2025-05-19 18:53:06 -0300

    CONSTRUCT FILTER

--- cd-342/src/pages/General/General.js
+++ cd-342/src/pages/General/General.js
@@ -1,8 +1,37 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+
 import Sessions from "./Sessions/Sessions"
 
 import styles from './General.module.css'
 
 function General() {
+
+    const [dados, setDados] = useState([])
+    const [error, setError] = useState(null)
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:5000/api/dados')
+            setDados(response.data)
+        } catch (err) {
+            setError(err.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const dadosAgrupados = dados.reduce((acc, item) => {
+        const categoria = item.categoria || 'Sem categoria'
+
+        if (!acc[categoria]) {
+            acc[categoria] = []
+        }
+        acc[categoria].push(item)
+        return acc
+    }, {})
 
     return (
         <section>
@@ -12,7 +41,7 @@ function General() {
                         <legend className={styles.category}>
                             Impressoras Laser
                         </legend>
-                        <Sessions categoria='Impressoras Laser' nome='HP' ip='192.0.2.123' mac='DE:AD:BE:EF:00:01' patrimônio='000123' setor='RH' />
+                        <Sessions nome='HP' ip='192.0.2.123' mac='DE:AD:BE:EF:00:01' patrimônio='001230' setor='RH' />
                     </fieldset>
                 </li>
                 <li>
@@ -20,7 +49,7 @@ function General() {
                         <legend className={styles.category}>
                             Impressoras Térmicas
                         </legend>
-                        <Sessions nome='Argox' ip='192.0.2.123' mac='DE:AD:BE:EF:00:01' patrimônio='000123' setor='CIDADE' />
+                        <Sessions nome='Argox' ip='192.0.2.123' mac='DE:AD:BE:EF:00:01' patrimônio='002230' setor='CIDADE' />
                     </fieldset>
                 </li>
                 <li>
@@ -28,7 +57,7 @@ function General() {
                         <legend className={styles.category}>
                             Computadores
                         </legend>
-                        <Sessions categoria='Computadores' nome='INTEL' ip='192.0.2.123' mac='DE:AD:BE:EF:00:01' patrimônio='000123' setor='TI' />
+                        <Sessions nome='INTEL' ip='192.0.2.123' mac='DE:AD:BE:EF:00:01' patrimônio='003230' setor='TI' />
                     </fieldset>
                 </li>
             </ul>
